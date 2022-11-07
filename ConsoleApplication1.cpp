73a8dbe7b0d97ce8fdfc960625c495be4f26eec5
Adam Nowak <kamiloles14@gmail.com>
2022-11-07 11:13:15 +0100

    przeliczania między postaciami funkcji kwadratowej 3.0

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -6,11 +6,13 @@ using namespace std;
 
 int main()
 {
-	double a, b,c,d,e,f;
+	double a, b,c,d,e,f,g,p,q;
 	cout << "Jaka funkcje licze wybiez: \n";
 	cout << "1.kwadratowa  \n";
 	cout << "2.liniowa  \n";
 	cout << "3.postac kanoniczna f.kwadratowa   \n";
+	cout << "4.zamiana postaci funkcji kwadratowa   \n";
+
 	cin >> f;
 	if (f==1)
 	{
@@ -74,6 +76,95 @@ int main()
 		cout << "\nmiejsce zerowe to x = " << e;
 		e = b - sqrt(-c / a);
 		cout << "\nmiejsce zerowe to x = " << e;
+
+
+	}
+	if (f == 4)
+	{
+		cout << "\nZ jakiej funkcji na jaką chcesz zamienic? " ;
+		cout << "\n1.Ogolna -> kanoniczna ";
+		cout << "\n2.Ogolna -> iloczynowa ";
+		cout << "\n3.Kanoniczna -> iloczynowa ";
+		cout << "\n4.Kanoniczna -> ogólna ";
+		cout << "\n5.Iloczynowa -> kanoniczna ";
+		cout << "\n6.Iloczynowa -> ogólna ";
+		cin >> g;
+		if (g==1 || g==2)
+		{
+			cout << "Podaj wspołczynnik a funkcji w postaci ax2 + bx + c \n";
+			cin >> a;
+
+			cout << "Podaj wspołczynnik b funkcji w postaci ax2 + bx + c \n";
+			cin >> b;
+			cout << "Podaj wspołczynnik c funkcji w postaci ax2 + bx + c \n";
+			cin >> c;
+			d = b * b - 4 * a * c;
+			if (g==1)
+			{
+
+			p = -b / 2 * a;
+			q = -sqrt(d) / 4 * a;
+			cout << "Postac kanoniczna  funkcji wynosi f(x)= "<<a<<"(x-"<<p<<")^2 + "<<q<<" \n";
+			}
+			if (g == 2)
+			{
+				p = (-b - sqrt(d)) / 2 * a;
+				q = (-b + sqrt(d)) / 2 * a;
+
+				cout << "Postac iloczynowa  funkcji wynosi f(x)= " << a << "(x-" << p << ") * (x-" << q << ") \n";
+			}
+
+
+		}
+		if (g == 3 || g == 4)
+		{
+			cout << "Podaj wspołczynnik a funkcji w postaci y= a(x-p)^2+q \n";
+			cin >> a;
+
+			cout << "Podaj wspołczynnik p funkcji w postaci y= a(x-p)^2+q\n";
+			cin >> p;
+			cout << "Podaj wspołczynnik q funkcji w postaci y= a(x-p)^2+q\n";
+			cin >> q;
+			b = -2 * a*p;
+			c = a * p * 2 + q;
+			if (g==4)
+			{
+
+			cout << "Postac ogolna  funkcji wynosi f(x)= " << a << "x^2 +" <<b << "x +" << c << " \n";
+			}
+			if (g == 3)
+			{
+				d = b * b - 4 * a * c;
+				p = (-b - sqrt(d)) / 2 * a;
+				q = (-b + sqrt(d)) / 2 * a;
+
+				cout << "Postac iloczynowa  funkcji wynosi f(x)= " << a << "(x-" << p << ") * (x-" << q << ") \n";
+			}
+		}
+		if (g == 5 || g == 6)
+		{
+			cout << "Podaj wspołczynnik a funkcji w postaci y= a(x-x1)*(x-x2) \n";
+			cin >> a;
+			cout << "Podaj wspołczynnik x1 funkcji w postaci y= a(x-x1)*(x-x2) \n";
+			cin >> p;
+			cout << "Podaj wspołczynnik x2 funkcji w postaci y= a(x-x1)*(x-x2) \n";
+			cin >> q;
+			b = a * (-p - q);
+			c = a * (p + q);
+			if (g == 6)
+			{
+
+				cout << "Postac ogolna  funkcji wynosi f(x)= " << a << "x^2 +" << b << "x +" << c << " \n";
+			}
+			if (g == 5)
+			{
+				d = b * b - 4 * a * c;
+				p = -b / 2 * a;
+				q = -sqrt(d) / 4 * a;
+				cout << "Postac kanoniczna  funkcji wynosi f(x)= " << a << "(x-" << p << ")^2 + " << q << " \n";
+			}
+
+		}
 
 
 	}
